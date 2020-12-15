cec80b5eeab9ee9df34e8adcdfd6f24c37948b9e
Andrew Pham <andrewpham.vk@gmail.com>
2020-12-14 21:25:34 -0800

    p15

---
+++ 15/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "input.txt"));
+            var initNums = input.Split(",").Select(x => int.Parse(x)).ToArray();
+            var p1 = 0;
+            var p2 = 0;
+
+            var mem = new Dictionary<int, List<int>>();
+            for (int i = 0; i < initNums.Length; i++)
+            {
+                mem.Add(initNums[i], new List<int> { i });
+            }
+
+            if (!mem.ContainsKey(0))
+            {
+                mem.Add(0, new List<int>());
+            }
+
+            int lastVal = initNums.Last();
+            for (int i = initNums.Length; i < 30000000; i++)
+            {
+                // System.Console.WriteLine(lastVal);
+                lastVal = GetLastAndAdd(mem, lastVal, i);
+            }
+
+            System.Console.WriteLine($"{lastVal} {p2}");
+        }
+
+        static int GetLastAndAdd(Dictionary<int, List<int>> mem, int lastVal, int idxNum)
+        {
+            if (!mem.ContainsKey(lastVal))
+            {
+                mem.Add(lastVal, new List<int>());
+            }
+
+            if (mem[lastVal].Count < 2)
+            {
+                mem[0].Add(idxNum);
+                return 0;
+            }
+
+            int res = mem[lastVal][mem[lastVal].Count - 1] - mem[lastVal][mem[lastVal].Count - 2];
+            if (!mem.ContainsKey(res))
+            {
+                mem.Add(res, new List<int>());
+            }
+            mem[res].Add(idxNum);
+            return res;
+        }
+    }
+}
