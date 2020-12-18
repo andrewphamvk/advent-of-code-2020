1e72e7e0359e80d7fb4d5848927020b040f66bc9
Andrew Pham <andrewpham.vk@gmail.com>
2020-12-18 15:58:43 -0800

    I

--- 18/Program.cs
+++ 18/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.FSharp.Core;
 using static FParsec.CSharp.PrimitivesCS; // combinator functions
 using static FParsec.CSharp.CharParsersCS; // pre-defined parsers
 using Soukoku.ExpressionParser;
+using System.Collections.Generic;
 
 namespace _18
 {
@@ -20,77 +21,85 @@ namespace _18
             var input = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "input.txt"));
 
             PartOne(input);
-            PartTwo(input);
+            // PartTwo(input);
 
             Console.WriteLine($"{p1} {p2}");
         }
 
         static void PartOne(string[] input)
         {
-            var context = new EvaluationContext();
-            var evaluator = new Evaluator(context);
-            var result = evaluator.Evaluate("33 + 55 * 2");
-            Console.WriteLine(result.ToString()); // should be "88"
-
-            // var basicExprParser = new OPPBuilder<Unit, long, Unit>()
-            //     .WithOperators(ops => ops
-            //     .AddInfix("+", 1, (x, y) => x + y)
-            //     .AddInfix("*", 1, (x, y) => x * y))
-            //     .WithTerms(term => Choice(Long, Between('(', term, ')')))
-            //     .Build()
-            //     .ExpressionParser;
-
-            // foreach (var line in input)
-            // {
-            //     var expr = line.Replace(" ", string.Empty);
-            //     var res = basicExprParser.Run(expr).GetResult();
-            //     p1 += res;
-            // }
+            foreach (var line in input)
+            {
+                System.Console.WriteLine(Calculate(line));
+            }
         }
 
-        // 0 = NoOp
-        // 1 = Addition
-        // 2 = Multiplication
-        // After running this func, start will be the end of the expr
-        static (long res, int end) Calculate(string expr, int start)
+        static int Calculate(string s)
         {
-            long val = 0;
-            if (Char.IsDigit(expr[start]))
-            {
-                val = expr[start++] - '0';
-            }
-            else if (expr[start] == '(')
-            {
-                var (initRes, end) = Calculate(expr, start + 1);
-                val = initRes;
-                start = end;
-            }
+            s = s.Replace(" ", string.Empty);
+            int res = 0;
+            var stack = new Stack<object>();
 
-            if (start < expr.Length)
+            int operand = 0;
+            int sign = 1;
+            for (int i = 0; i < s.Length; i++)
             {
-                if (expr[start] == '+')
+                if (char.IsDigit(s[i]))
                 {
-                    start++;
-                    var (intRes, end) = Calculate(expr, start);
-                    val += intRes;
-                    start = end;
+                    operand *= 10;
+                    operand += s[i] - '0';
                 }
-                else if (expr[start] == '*')
+                else
                 {
-                    start++;
-                    var (intRes, end) = Calculate(expr, start);
-                    val *= intRes;
-                    start = end;
+                    if (s[i] == '+')
+                    {
+                        res += sign * operand;
+                        sign = 1;
+                        operand = 0;
+                    }
+                    else if (s[i] == '-')
+                    {
+                        res += sign * operand;
+                        sign = -1;
+                        operand = 0;
+                    }
+                    else if (s[i] == '(')
+                    {
+                        stack.Push(res);
+                        stack.Push(sign);
+                        res = 0;
+                        sign = 1;
+                    }
+                    else if (s[i] == ')')
+                    {
+                        res += sign * operand;
+                        res *= (int)stack.Pop();
+                        res += (int)stack.Pop();
+                        operand = 0;
+                    }
                 }
             }
 
-            if (start < expr.Length && expr[start] == ')')
+
+            return res + (sign * operand);
+        }
+
+        static int EvaluateExpr(Stack<object> stack)
+        {
+            int res = 0;
+            if (stack.Any())
             {
-                start++;
+                res = (int)stack.Pop();
             }
 
-            // System.Console.WriteLine($"res: {val}");
-            return (val, start);
+            while (stack.Any() && (char)stack.Peek() != ')')
+            {
+                char sign = (char)stack.Pop();
+                if (sign == '+') res += (int)stack.Pop();
+                if (sign == '-') res -= (int)stack.Pop();
+            }
+
+            return res;
         }
 
         static void PartTwo(string[] input)
