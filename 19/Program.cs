20927e4201def3ff55fa864b11781449916613ff
Andrew Pham <andrewpham.vk@gmail.com>
2020-12-20 16:38:17 -0800

    Stash

--- 19/Program.cs
+++ 19/Program.cs
@@ -23,12 +23,135 @@ namespace _19
 
         static void PartOne(string[] input)
         {
+            int lineNum = 0;
+            var ruleMap = new Dictionary<int, Rule>();
+            while (!string.IsNullOrWhiteSpace(input[lineNum]))
+            {
+                var split1 = input[lineNum++].Split(": ");
+                var split2 = split1[1].Split(" | ");
 
+                int ruleNum = int.Parse(split1[0]);
+                Rule rule = GetOrAdd(ruleMap, ruleNum);
+
+                for (int i = 0; i < split2.Length; i++)
+                {
+                    var ruleSplit = split2[i].Split(" ");
+                    if (ruleSplit[0] == "\"a\"" )
+                    {
+                        rule.Word = "a";
+                    }
+                    else if (ruleSplit[0] == "\"b\"")
+                    {
+                        rule.Word = "b";
+                    }
+                    else
+                    {
+                        var dependsOn = new List<Rule>();
+                        foreach (var r in ruleSplit)
+                        {
+                            dependsOn.Add(GetOrAdd(ruleMap, int.Parse(r)));
+                        }
+                        rule.DependsOn.Add(dependsOn);
+                    }
+                }
+            }
+
+            lineNum++;
+            var rule0 = ruleMap[0];
+            var valid = new HashSet<string>(rule0.GetMessages());
+
+            Console.Write($"42: ");
+            foreach (var r in ruleMap[42].GetMessages())
+            {
+                Console.Write($"{r} ");
+            }
+            Console.WriteLine();
+
+            Console.Write($"31: ");
+            foreach (var r in ruleMap[31].GetMessages())
+            {
+                Console.Write($"{r} ");
+            }
+            Console.WriteLine();
+
+            while (lineNum < input.Length)
+            {
+                var toValidate = input[lineNum++];
+                if (valid.Contains(toValidate)) p1++;
+            }
+        }
+
+        static Rule GetOrAdd(Dictionary<int, Rule> ruleMap, int ruleNum)
+        {
+            Rule rule = null;
+            if (ruleMap.ContainsKey(ruleNum))
+            {
+                rule = ruleMap[ruleNum];
+            }
+            else
+            {
+                rule = new Rule { Number = ruleNum };
+                ruleMap.Add(ruleNum, rule);
+            }
+
+            return rule;
         }
 
         static void PartTwo(string[] input)
         {
 
+        }
+    }
+
+    class Rule
+    {
+        public int Number;
+
+        public List<List<Rule>> DependsOn = new List<List<Rule>>();
+
+        public string Word = null;
+
+        private List<string> messages = null;
+
+        public List<string> GetMessages()
+        {
+            if (this.messages != null) return this.messages;
+
+            if (this.Word != null)
+            {
+                this.messages = new List<string> { this.Word };
+                return this.messages;
+            }
+
+            this.messages = new List<string>();
+            foreach (var group in DependsOn)
+            {
+                if (group.Count == 2)
+                {
+                    this.messages.AddRange(Merge(group[0].GetMessages(), group[1].GetMessages()));
+                }
+                else
+                {
+                    this.messages.AddRange(group[0].GetMessages());
+                }
+            }
+
+            return this.messages;
+        }
+
+        public static List<string> Merge(List<string> a, List<string> b)
+        {
+            var res = new List<string>();
+
+            foreach (var na in a)
+            {
+                foreach (var nb in b)
+                {
+                    res.Add(na + nb);
+                }
+            }
+
+            return res;
         }
     }
 }
---
+++ 20/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _20
+{
+    class Program
+    {
+        private static int p1 = default;
+        private static int p2 = default;
+
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "input.txt"));
+
+            PartOne(input);
+            PartTwo(input);
+
+            Console.WriteLine($"{p1} {p2}");
+        }
+
+        static void PartOne(string[] input)
+        {
+            var tiles = new List<Tile>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var tileLine = input[i++];
+                var match = Regex.Match(tileLine, @"Tile (?<Id>\d+):");
+                
+                var tile = new Tile();
+                tile.Id = int.Parse(match.Groups["Id"].Value);
+                var grid = new List<char[]>();
+                while (!string.IsNullOrWhiteSpace(input[i]))
+                {
+                    grid.Add(input[i].ToCharArray());
+                    i++;
+                }
+
+                tile.Grid = grid.ToArray();
+                tile.Init();
+                tiles.Add(tile);
+            }
+
+            var tile0 = tiles[0];
+            Print(tile0.Grid);
+            tile0.Rotate(0);
+            Console.WriteLine(tile0.Get(Dir.Top));
+            tile0.Rotate(1);
+            Console.WriteLine(tile0.Get(Dir.Top));
+            tile0.Rotate(2);
+            Console.WriteLine(tile0.Get(Dir.Top));
+            tile0.Rotate(3);
+            Console.WriteLine(tile0.Get(Dir.Top));
+
+            Console.WriteLine();
+            tile0.Flip(true);
+
+            tile0.Rotate(0);
+            Console.WriteLine(tile0.Get(Dir.Top));
+            tile0.Rotate(1);
+            Console.WriteLine(tile0.Get(Dir.Top));
+            tile0.Rotate(2);
+            Console.WriteLine(tile0.Get(Dir.Top));
+            tile0.Rotate(3);
+            Console.WriteLine(tile0.Get(Dir.Top));
+
+            bool[] used = new bool[tiles.Count];
+            var curr = new Tile[tiles.Count];
+            Backtrack(tiles, used, curr, 0);
+
+        }
+
+        static void Print(char[][] grid)
+        {
+            var sb = new StringBuilder();
+            foreach (var g in grid)
+            {
+                sb.Append(g);
+                sb.AppendLine();
+            }
+
+            Console.WriteLine(sb);
+            Console.WriteLine();
+        }
+
+        static bool Backtrack(List<Tile> tiles, bool[] used, Tile[] curr, int idx) 
+        {
+            if (idx == tiles.Count) {
+                System.Console.WriteLine($"Found {curr[0].Id} {curr[11].Id} {curr[132].Id} {curr[143].Id}");
+                return true;
+            }
+
+            p2 = Math.Max(p2, idx);
+
+            for (int i = 0; i < tiles.Count; i++)
+            {
+                var tile = tiles[i];
+                if (!used[i])
+                {
+                    used[i] = true;
+                    curr[idx] = tile;
+                    // Try rotating
+                    for (int r = 0; r < 4; r++)
+                    {
+                        tile.Rotate(r);
+                        for (int f = 0; f < 2; f++)
+                        {
+                            bool flip = f == 1;
+                            tile.Flip(flip);
+
+                            if (idx % 12 != 0 && curr[idx - 1].Get(Dir.Right) != tile.Get(Dir.Left))
+                            {
+                                continue;
+                            }
+
+                            if (idx > 11 && curr[idx - 12].Get(Dir.Bot) != tile.Get(Dir.Top))
+                            {
+                                continue;
+                            }
+
+
+                            if (Backtrack(tiles, used, curr, idx + 1)) return true;
+
+                        }
+                    }
+
+                    tile.Rotate(0);
+                }
+
+                curr[idx] = null;
+                used[i] = false;
+            }
+
+            return false;
+        }
+
+        static void PartTwo(string[] input)
+        {
+
+        }
+    }
+
+    public class Tile
+    {
+        public int Id;
+        public char[][] Grid;
+        private int Rotation = 0;
+        private bool flip = false;
+        private string[] borders = new string[4];
+
+        public void Init()
+        {
+            borders[(int)Dir.Top] = new String(this.Grid[0]);
+            borders[(int)Dir.Bot] = new String(this.Grid[^1]);
+
+            var sbLeft = new StringBuilder();
+            var sbRight = new StringBuilder();
+            foreach (var r in this.Grid)
+            {
+                sbLeft.Append(r[0]);
+                sbRight.Append(r[^1]);
+            }
+
+            borders[(int)Dir.Left] = sbLeft.ToString();
+            borders[(int)Dir.Right] = sbRight.ToString();
+        }
+
+        public void Rotate(int num)
+        {
+            Rotation = num;
+            //Rotation = ((Rotation + 4) + num) % 4;
+        }
+
+        public void Flip(bool flip)
+        {
+            this.flip = flip;
+        }
+
+        public string Get(Dir dir)
+        {
+            int tempRotation = Rotation;
+            if (this.flip) tempRotation += 2;
+            int dirInt = (int)dir;
+            dirInt += tempRotation;
+
+            string res;
+            dirInt = dirInt % 4;
+
+            if (tempRotation == 1 && (dirInt == (int)Dir.Top || dirInt == (int)Dir.Bot))
+            {
+                res = new string(borders[dirInt].Reverse().ToArray());
+            }
+            else if (tempRotation == 2)
+            {
+                res = new string(borders[dirInt].Reverse().ToArray());
+            }
+            else if (tempRotation == 3 && (dirInt == (int)Dir.Left || dirInt == (int)Dir.Right))
+            {
+                res = new string(borders[dirInt].Reverse().ToArray());
+            }
+            else
+            {
+                res = borders[dirInt];
+            }
+
+            if (this.flip) res = new string(res.Reverse().ToArray());
+
+            return res;
+        }
+    }
+
+    public enum Dir
+    {
+        Top = 0,
+        Right = 1,
+        Bot = 2,
+        Left = 3
+    }
+}
